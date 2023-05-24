e94f65473c2fae14a4c55a670f767e6261cbf5a1
kiraGit67 <kirsten@sais.de>
2023-05-24 22:57:50 +0200

    feat: adds details-, colors- and sizes-list and list rendering

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ const app = Vue.createApp({
       alt: "Green Socks",
       inStock: false,
       inventory: 25,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: [
+        { id: "XS", description: "xtraSmall" },
+        { id: "S", description: "small" },
+        { id: "M", description: "medium" },
+        { id: "L", description: "large" },
+        { id: "XL", description: "xtraLarge" },
+        { id: "XXL", description: "doubleXtraLarge" },
+      ],
     };
   },
 });
